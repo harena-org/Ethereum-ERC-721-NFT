e2bf180f6d78432ce32327758c95b9fb1a2cf4d1
emma <emma@dev-team.com>
2026-03-11 08:23:56 +0000

    feat: implement MyNFT ERC-721 contract with mintBatch

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract MyNFT is ERC721, Ownable {
+    using Strings for uint256;
+
+    uint256 private _nextTokenId = 1;
+    string private _baseTokenURI;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        string memory baseTokenURI
+    ) ERC721(name, symbol) Ownable(msg.sender) {
+        _baseTokenURI = baseTokenURI;
+    }
+
+    function mintBatch(address to, uint256 quantity) external onlyOwner {
+        for (uint256 i = 0; i < quantity; i++) {
+            _mint(to, _nextTokenId);
+            _nextTokenId++;
+        }
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        _requireOwned(tokenId);
+        return string.concat(_baseTokenURI, tokenId.toString());
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _nextTokenId - 1;
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      evmVersion: "cancun",
+    },
+  },
   networks: {
     hardhat: {},
     mainnet: {
